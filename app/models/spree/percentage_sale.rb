d995e71700bf52ff566da5402588a386d0677d4d
Herman Moreno <herman.moreno@crowdint.com>
2013-09-17 15:55:07 -0500

    Add validations

--- app/models/spree/percentage_sale.rb
+++ app/models/spree/percentage_sale.rb
@@ -2,5 +2,8 @@ module Spree
   class PercentageSale < Sale
     has_many :percentage_sales_variants, foreign_key: :sale_id
     has_many :variants, through: :percentage_sales_variants
+
+    validates :value, :numericality => { :less_than_or_equal_to => 100.0 }
+
   end
 end
--- app/models/spree/sale.rb
+++ app/models/spree/sale.rb
@@ -5,11 +5,23 @@ module Spree
     has_many :sales_variants
     has_many :variants, through: :sales_variants
 
+    validates :start_at, presence: true
+    validates :end_at,
+              presence: true
+              :cannot_be_before_start_at
+
     attr_accessible :variants, :start_at, :end_at, :value
 
     def active?
       start_at <= Time.zone.now && end_at >= Time.zone.now
     end
 
+    private
+
+    def cannot_be_before_start_at
+      if end_at < start_at
+        errors.add(:end_at, "end_at can't be before start_at")
+      end
+    end
   end
 end
